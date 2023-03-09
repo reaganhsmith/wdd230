8d9d555ca2f2e1ee603ddfc3e3d7da8052bdf971
reaganhsmith <reagansmith@byui.edu>
2023-03-09 13:21:33 -0700

    weather for week 10

---
+++ chamber/scripts/weather.js
@@ -0,0 +1,46 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#temp');
+const weatherIcon = document.querySelector('#weather-icon');
+
+const weatherDescription = document.querySelector('#weathertype');
+
+const windSpeed1 = document.querySelector('#windspeed');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=star&units=imperial&appid=c0299471663639e3035fb06977385833' ;
+
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
+
+  function  displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+  
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+  
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    weatherIcon.setAttribute('loading', 'lazy');
+
+    weather = weatherData.weather[0].description;
+    weatherDescription.innerHTML = `<p>${weather}</p>`;
+
+    console.log(weatherData.wind.speed.toFixed(0));
+    windSpeed1.textContent = `${weatherData.wind.speed.toFixed(0)}`;
+
+  }
+  
--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -6,8 +6,11 @@ const windChill = document.querySelector("#windchill");
 
 
 //These 2 lines of code convert the <span> into just text and then turn the vue into a number
-const temp = parseFloat(tempLocation.textContent);
-const windSpeed = parseFloat(windSpeedLocation.textContent);
+const temp = parseFloat(tempLocation.value);
+const windSpeed = parseFloat(windSpeedLocation.value);
+
+console.log(temp);
+console.log(windSpeed);
 
 //equation to get the windchill 
 let windChillAmount = 35.74 + (0.6215 * temp) - (35.75 * (windSpeed ** 0.16)) +(0.4275 * temp * (windSpeed ** 0.16));
