39fc9e046c75b17855593cb8fe26c5154ae3992c
reaganhsmith <reagansmith@byui.edu>
2023-04-03 10:23:29 -0600

    final touches

--- Final/scripts/formcontent.js
+++ Final/scripts/formcontent.js
@@ -3,7 +3,6 @@ const url = "https://brotherblazzard.github.io/canvas-content/fruit.json";
 async function getFruitData() {
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
     displayfruit(data);
     displayfruit2(data);
     displayfruit3(data);
