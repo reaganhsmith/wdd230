3567217429fbfb4c714965bf13ee2920c88de6e6
reaganhsmith <reagansmith@byui.edu>
2023-04-04 12:40:27 -0600

    changes

--- Bountfiful/scripts/created.js
+++
@@ -1,9 +0,0 @@
-// Check if the form has been submitted before
-if (localStorage.getItem('formSubmissions') === null) {
-  localStorage.setItem('formSubmissions', 0);
-}
-
-// Add event listener to the form 
-document.querySelector('.submitBtn').addEventListener('click', function() {
-localStorage.setItem('formSubmissions', parseInt(localStorage.getItem('formSubmissions')) + 1);
-});
--- Bountfiful/scripts/nav.js
+++
@@ -1,8 +0,0 @@
-//for my navigation menu
-function toggleMenu(){
-    document.querySelector('#primaryNav').classList.toggle('open');
-    document.querySelector('#hamburgerBtn').classList.toggle('open');
-}
-
-const hamButton =document.querySelector('#hamburgerBtn');
-hamButton.onclick = toggleMenu;
